60f0f4f5c77f262d1288cd9f704469d021d8fcd8
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2024-01-24 21:38:54 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+let scrollContainer = document.querySelector(".gallery");
+let btnLeft = document.getElementById("btnleft");
+let btnRight = document.getElementById("btnright");
+
+let scrollContainer1 = document.querySelector(".gallery1");
+let btnLeft1 = document.getElementById("btnleft1");
+let btnRight1 = document.getElementById("btnright1");
+
+scrollContainer.addEventListener("wheel", (evt) =>{
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior ="auto";
+})
+btnRight.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior ="smooth";
+    scrollContainer.scrollLeft += 280
+})
+btnLeft.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior ="smooth";
+    scrollContainer.scrollLeft -= 280
+})
+scrollContainer1.addEventListener("wheel", (evt) =>{
+    evt.preventDefault();
+    scrollContainer1.scrollLeft += evt.deltaY;
+    scrollContainer1.style.scrollBehavior ="auto";
+})
+btnRight1.addEventListener("click",()=>{
+    scrollContainer1.style.scrollBehavior ="smooth";
+    scrollContainer1.scrollLeft += 280
+})
+btnLeft1.addEventListener("click",()=>{
+    scrollContainer1.style.scrollBehavior ="smooth";
+    scrollContainer1.scrollLeft -= 280
+})
